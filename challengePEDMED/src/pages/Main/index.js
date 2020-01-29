8b73639349a34bfe09f660fbeb5612c72393b6c6
Hannah Lisboa <hannahlisboa@outlook.com>
2020-01-29 13:00:00 -0300

    create component accordian

--- challengePEDMED/src/pages/Main/index.js
+++ challengePEDMED/src/pages/Main/index.js
@@ -1,11 +1,57 @@
 import React, {Component} from 'react';
 
-import {View} from 'react-native';
+import {FlatList, Text, View, SafeAreaView, SectionList} from 'react-native';
+import {getData} from '../../store/actions/indications';
+import {connect} from 'react-redux';
+import Accordian from '../../components/Accordian';
 
-// import { Container } from './styles';
-
-export default class Main extends Component {
+class Main extends Component {
+  componentDidMount() {
+    this.props.onGetData();
+  }
   render() {
-    return <View />;
+    const {listIndications} = this.props;
+    return (
+      <SafeAreaView>
+        {/* <SectionList
+          sections={listIndications}
+          keyExtractor={(item, index) => item + index}
+          renderItem={({item}) => {
+            return <Text>{item.name}</Text>;
+          }}
+          renderSectionHeader={({section: {title}}) => (
+            <Text
+              style={{
+                fontSize: 20,
+                fontWeight: 'bold',
+                padding: 30,
+              }}>
+              {title}
+            </Text>
+          )}
+        /> */}
+        <FlatList
+          data={listIndications}
+          renderItem={({item}) => {
+            console.log('ITEM', item);
+            return <Accordian title={item.title} data={item.data} />;
+          }}
+          keyExtractor={(item, index) => item.title}
+        />
+      </SafeAreaView>
+    );
   }
 }
+
+const mapStateToProps = ({indications}) => ({
+  listIndications: indications.listIndications,
+});
+
+const mapDispatchToProps = dispatch => ({
+  onGetData: () => dispatch(getData()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Main);
