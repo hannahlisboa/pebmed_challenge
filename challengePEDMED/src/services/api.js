4b73c5dbd3625818ce7b4a34d53c36771bd2e257
Hannah Lisboa <hannahlisboa@outlook.com>
2020-01-29 12:58:57 -0300

    creat conection api pedmed

--- challengePEDMED/src/services/api.js
+++ challengePEDMED/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'https://api.github.com',
-});
-
-export default api;
+const BASE_URL = 'https://private-54fe53-pebmeddesafio.apiary-mock.com';
+export const pedMedApi = (sucess, error) => {
+  const request = axios({
+    method: 'GET',
+    url: `${BASE_URL}/contents`,
+    headers: [],
+  });
+  return request.then(response => sucess(response), err => error(err));
+};
