a517f7beaedfa18484300baf29d89427fcb16a47
Hannah Lisbôa <hannahlisboa@tvescola.org.br>
2020-01-29 18:37:33 -0300

    create page Detail

--- challengePEDMED/src/pages/Detail/index.js
+++ challengePEDMED/src/pages/Detail/index.js
@@ -1,16 +1,43 @@
 import React, {Component} from 'react';
 
-import {View} from 'react-native';
+import {View, Text, ScrollView} from 'react-native';
 import ImageCustom from '~/components/ImageCustom';
 import styles from './styles';
+
 export default class Detail extends Component {
+  static navigationOptions = ({navigation}) => {
+    return {
+      headerTitle: (
+        <Text style={[styles.text, {color: 'white'}]}>
+          {navigation.getParam('item').name}
+        </Text>
+      ),
+      headerTintColor: '#ffffff',
+      headerTransparent: true,
+      headerStyle: {borderBottomWidth: 0},
+    };
+  };
   render() {
     const {navigation} = this.props;
     const item = navigation.getParam('item');
     return (
-      <View style={{flex: 1, backgroundColor: 'red'}}>
-        <ImageCustom url={item.urlImage} style={styles.thumb} />
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <ImageCustom url={item.urlImage} style={styles.thumb} />
+          <View style={styles.container_data}>
+            <Text style={[styles.text, {marginBottom: 20}]}>DESCRIÇÃO</Text>
+            <Text style={styles.text_description}>{item.description}</Text>
+            <Text style={[styles.text, {marginTop: 20}]}>
+              REFERÊNCIAS BIBLIOGRÁFICAS
+            </Text>
+            {item.authors.map(author => (
+              <Text style={[styles.text_description, {marginTop: 20}]}>
+                {author.name}
+              </Text>
+            ))}
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
--- challengePEDMED/src/pages/Detail/styles.js
+++ challengePEDMED/src/pages/Detail/styles.js
@@ -5,4 +5,17 @@ export default StyleSheet.create({
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').width * 0.75,
   },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  text_description: {
+    color: 'gray',
+  },
+  container_data: {
+    padding: 20,
+  },
+  container: {
+    flex: 1,,
+  },
 });
